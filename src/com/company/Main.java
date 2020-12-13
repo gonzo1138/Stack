295458a166477d89c7daa856c1e993dec7a42c85
Thomas <gonzo113@bassfreaks.de>
2020-12-13 22:45:49 +0100

    added search() and hole presentation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-
         stack.push(1);                                         // Methode push
         stack.push(4);
         stack.push(9);
-
+        int n = 4;
+        System.out.println("Die " + n + " befindet sich an Stelle " + stack.search(n)); // Methode search
         System.out.println("Oberstes Element: " + stack.peek());    // Methode peek
-
         stack.pop();                                                // Methode pop
-        stack.pop();
-
+        System.out.println(stack.pop());
         System.out.println("Der Stack hat " + stack.size() + " Element(e)");  // Methode size
-
         System.out.println(stack.empty() ? "Stack ist leer" : "Stack ist gefüllt"); // Methode empty
     }
 }
